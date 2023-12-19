c1dbcc26e66661982467e036b0c52487b356d427
TechAccessories <154078661+TechAccessories@users.noreply.github.com>
2023-12-19 07:51:52 +0100

    header mods

--- JS/header.js
+++ JS/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("../../header/header.html")
+    fetch("../header.html")
         .then(response => response.text())
         .then(html => {
             document.querySelector("body").insertAdjacentHTML("afterbegin", html);
