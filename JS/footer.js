f3bd4ead978ca9b283cf1ab5cb3d722af8c17076
TechAccessories <154078661+TechAccessories@users.noreply.github.com>
2023-12-19 15:28:30 +0100

    delete everything

--- JS/footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("../../footer/footer.html")
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector("body").insertAdjacentHTML("beforeend", html);
-        });
-});
--- JS/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("../header.html")
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector("body").insertAdjacentHTML("afterbegin", html);
-        });
-});
-
