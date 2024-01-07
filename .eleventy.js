ae02feb03966cd1b3ac9fa805782fe5c73550ddb
TechAccessories <154078661+TechAccessories@users.noreply.github.com>
2024-01-07 13:08:14 +0100

    dynamics

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('./css');
+    eleventyConfig.addPassthroughCopy('./images');
+    eleventyConfig.addPassthroughCopy('./js');
+    eleventyConfig.addPassthroughCopy('./header/header.css');
+    eleventyConfig.addPassthroughCopy('./footer/footer.css');
+    
+}
